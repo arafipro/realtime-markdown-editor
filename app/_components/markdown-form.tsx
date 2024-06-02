05de88fbf3085a5d596f45709305f51b58222326
Hiroki Chiyoda <contact@arafipro.com>
2024-06-02 17:01:02 +0900

    06_add-md-form-comp

---
+++ app/_components/markdown-form.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { Textarea } from "@/components/ui/textarea";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const schema = z.object({
+  content: z.string().min(2),
+});
+
+export default function MarkdownForm() {
+  const form = useForm<z.infer<typeof schema>>({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      content: "",
+    },
+  });
+
+  const onSubmit = (data: z.infer<typeof schema>) => {
+    console.log(data);
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="content"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Textarea {...field} />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <Button type="submit">送信</Button>
+      </form>
+    </Form>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import MarkdownForm from "./_components/markdown-form";
+
 export default function Home() {
-  return <main className="">Home</main>;
+  return (
+    <main className="container mx-auto mt-4">
+      <MarkdownForm />
+    </main>
+  );
 }
